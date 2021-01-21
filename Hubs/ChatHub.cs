2fceda15e2e843e1d2ff7e7ba59ec4517fcfe8ca
William Olewnicki <wolewnicki@yahoo.com>
2021-01-20 23:13:55 -0500

    started setting up saves to DB from SignalR

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,13 +1,29 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using app_messager.Services;
+using app_messager.Models;
 
 namespace app_messager.Hubs
 {
     public class ChatHub: Hub
     {
+
+        private readonly MessagerDBContext _dbContext;
+
+        public ChatHub(MessagerDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("RecieveMessage", user, message);
+            var sentMessage = Clients.All.SendAsync("RecieveMessage", user, message);
+            var newMessage = new Message() { sender = user, body = message};
+            _dbContext.Add(newMessage);
+            var awaitSave = _dbContext.SaveChangesAsync();
+
+            await Task.WhenAll(awaitSave, sentMessage);
+
         }
     }
 }
